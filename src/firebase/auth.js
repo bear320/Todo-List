4b1dd735c5fbf9426caa23736fd2c515622a4a89
bear320 <aaabear320@gmail.com>
2023-03-05 03:56:11 +0800

    重新部署

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUsz3Bk2fejDwoxiAd9U7uLP_349BCE6c",
+    apiKey: process.env.VUE_FIREBASE_apiKey,
     authDomain: "todolist-2395c.firebaseapp.com",
     databaseURL: "https://todolist-2395c-default-rtdb.firebaseio.com",
     projectId: "todolist-2395c",
     storageBucket: "todolist-2395c.appspot.com",
-    messagingSenderId: "860096240753",
-    appId: "1:860096240753:web:4b10b51bb174ad2c090fd0",
+    messagingSenderId: process.env.VUE_FIREBASE_messagingSenderId,
+    appId: process.env.VUE_FIREBASE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
